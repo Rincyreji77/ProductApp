e734046087299e324f0b8d68819efb1c10abd294
rincyreji <rincy667@gmail.com>
2022-05-18 10:44:18 +0530

    message3

--- src/component/SearchProduct.js
+++ src/component/SearchProduct.js
@@ -1,16 +1,17 @@
 import React from 'react'
+import Header from './Header'
 
 const SearchProduct = () => {
   return (
     <div>
-
+<Header/>
 <div>
     <div class="container">
          <div class="row">
             <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                  <div class="row g-3">
                  <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <label for="" class="form-label">Book Title</label>
+                    <label for="" class="form-label">Product Name</label>
                     <input type="text" class="form-control"/>
                 </div>
                 
